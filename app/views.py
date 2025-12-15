ee3f6b64f8a3419b6a77fa2e7d062934f25ea735
dusanrsc <dusanrosic25.06.1997@gmail.com>
2025-12-15 23:30:13 +0100

    Half Done!

--- app/views.py
+++ app/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	context = {}
-	return render(request, "index.html", context)
+	index_context = {}
+	return render(request, "index.html", index_context)
+
+def about(request):
+	about_context = {}
+	return render(request, "about.html", about_context)
+
+def contact(request):
+	contact_context = {}
+	return render(request, "contact.html", contact_context)
